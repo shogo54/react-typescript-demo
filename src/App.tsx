8386b1b4414ba0d5c2c7e0b3263bcda936b386d4
Shogo Akiyama <sakiyama@knox.edu>
2021-05-29 13:59:09 -0700

    Get user input with Refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import NewTodo from './components/NewTodo';
+import TodoList from './components/TodoList';
+
 function App() {
+  const todos = [{ id: 't1', text: 'Finish the course' }];
   return (
     <div className="App">
+      <NewTodo />
+      <TodoList todos={todos} />
     </div>
   );
 }
