e39785720006b744ad176384bb5b0bb93cc4aab0
Shogo Akiyama <sakiyama@knox.edu>
2021-05-29 14:10:02 -0700

    Add todo state and types in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewTodo from './components/NewTodo';
 import TodoList from './components/TodoList';
+import { Todo } from './todo.model';
 
 function App() {
-  const todos = [{ id: 't1', text: 'Finish the course' }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos([{id: Math.random().toString(), text: text}]);
   };
 
   return (
