4094bf17b47768868cc2cbcd4e2f9122e82d939c
Shogo Akiyama <sakiyama@knox.edu>
2021-05-29 14:25:09 -0700

    Add delete function to the todo item

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 interface TodoListProps {
   todos: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = (props) => {
   return (
     <ul>
       {props.todos.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>DELETE</button>
+        </li>
       ))}
     </ul>
   );
